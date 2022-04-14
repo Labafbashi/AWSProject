99d4fa13563cf6cfd60abc632fce8f0ba58d0a04
paramont <paramont@gmail.com>
2022-04-14 12:53:41 +0200

    some changes

--- scripts/ApplicationStart.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd ~/AWSProject
-source venv/bin/activate
-pip install flask
-nohup python3 hello.py &
--- scripts/BeforeInstall.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-cd ~
-python3 -m pip install virtualenv
-cd AWSProject
-virtualenv -p /usr/bin/python3 venv
-
