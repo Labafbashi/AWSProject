0ab3b6521ba5b01067bf529b284dfe4c980fbd94
paramont <paramont@gmail.com>
2022-04-17 00:20:19 +0200

    Some Changes

--- Scripts/BeforeInstall.sh
+++ Scripts/BeforeInstall.sh
@@ -4,6 +4,6 @@ mkdir -p /home/ubuntu/AWSProject
 cd /home/ubuntu
 python3 -m pip install virtualenv
 cd AWSProject
-test -f codeDeploy.log && echo "file exist." || touch codeDeploy.log
+test ! -f codeDeploy.log && echo "file exist." || touch codeDeploy.log
 virtualenv -p /usr/bin/python3 venv
 
