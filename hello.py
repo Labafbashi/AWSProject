f3eb5bf7bf6729dd3ee889972e03a6bcc8518c4c
paramont <paramont@gmail.com>
2022-04-17 00:31:38 +0200

    Some Changes

--- hello.py
+++ hello.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Dear user! Here is a Flask App with AWS automation.</p><p><h1>Congratulation</h1></p><p><h3>EC Utbildning, AWS Final Project </h3></p><p>MohammadHosein Labafbashi</p>"
+    out = "<p>Hello, Dear user! Here is a Flask App with AWS automation.</p>"
+    out+= "<p><h1>Congratulation</h1></p>"
+    out+= "<p><h3>EC Utbildning, AWS Final Project </h3></p>"
+    out+= "<p>MohammadHosein Labafbashi</p>"
+    return out
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
