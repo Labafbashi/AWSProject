7b10e6fa28908a1eba00dcbed92bbfade27b02f0
paramont <paramont@gmail.com>
2022-04-16 16:31:57 +0200

    resolve the bug

--- hello.py
+++ hello.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "
-      <p>Hello, Dear user! Here is a Flask App with AWS automation.</p>
-      <p><h1>Congratulation</h1></p>
-      <p><h3>EC Utbildning, AWS Final Project </h3></p>
-      <p>MohammadHosein Labafbashi</p>
+    return " /
+      <p>Hello, Dear user! Here is a Flask App with AWS automation.</p> /
+      <p><h1>Congratulation</h1></p> /
+      <p><h3>EC Utbildning, AWS Final Project </h3></p> /
+      <p>MohammadHosein Labafbashi</p> /
     "
 
 if __name__ == "__main__":
