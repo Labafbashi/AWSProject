0c1f739e8d0fbf3e43b6bf02aaf5ae6a1ecb6203
paramont <paramont@gmail.com>
2022-04-16 15:31:52 +0200

    Some changes in Script

--- Scripts/ApplicationStart.sh
+++ Scripts/ApplicationStart.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-`cd /home/ubuntu/AWSProject`
+cd /home/ubuntu/AWSProject
 source venv/bin/activate
 sudo pip install flask
 sudo python3 hello.py >> codeDeploy.log 2>&1 &
--- Scripts/ApplicationStop.sh
+++ Scripts/ApplicationStop.sh
@@ -1,9 +1,9 @@
-#! /bin/sh
+#! /bin/bash
 
-if [[ $(ps -ef | grep python3|grep -v grep) ]]; then
-   `sudo pkill python3`
-    echo "Kill all python3 processes ...." >> codeDeploy.log
+if [[ $(ps -ef | grep hello.py|grep -v grep) ]]; then
+  sudo killall -9 python3
+  echo "Kill all python3 processes ...." >> codeDeploy.log
 else
-    echo "Hej, no process found!!!!!!!" >> codeDeploy.log
+  echo "Hej, no process found!!!!!!!" >> codeDeploy.log
 fi
 
