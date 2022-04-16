edbdc723c65c04d78621e0060054fb66db0ab040
paramont <paramont@gmail.com>
2022-04-16 16:50:05 +0200

    resolve the bug

--- Scripts/ApplicationStop.sh
+++ Scripts/ApplicationStop.sh
@@ -2,8 +2,5 @@
 
 if [[ $(ps -ef | grep hello.py|grep -v grep) ]]; then
   sudo killall -9 python3
-  echo "Kill all python3 processes ...." >> codeDeploy.log
-else
-  echo "Hej, no process found!!!!!!!" >> codeDeploy.log
 fi
 
