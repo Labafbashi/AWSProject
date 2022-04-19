bc86970644d3514042f383d5e4ff8060defc8ff9
paramont <paramont@gmail.com>
2022-04-19 11:00:18 +0200

    Changes on source code.

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ def hello_world():
     out+= "<p><h1>Congratulation</h1></p>"
     out+= "<p><h3>EC Utbildning, AWS Final Project </h3></p>"
     out+= "<p> Patrik </p>"
+    out+= "<p><h1> This is test for recording. </h1></p>"
     out+= "<p>MohammadHosein Labafbashi</p>"
     return out
 
